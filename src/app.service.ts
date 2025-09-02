477c86fece56cef9f9d7480cd1f5f8e62aeaf372
Juan <juanlopey22@gmail.com>
2025-09-02 19:45:05 -0300

    subiendo proyecto

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
+export interface iTrack { //para que se respete la estructura
+id:number;
+title:string;
+duration:number;
+artist:string;
+}
+
+export const tracks: iTrack[] = [ //no es lo ideal, es solo de prueba
+  { 
+    id:1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+    },
+    { 
+      id:2,
+      title: "cancion 2",
+      duration: 130,
+      artist: "interprete 2"
+      }
+
+]
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTraks(): iTrack[] {
+    return tracks;
   }
 }
+
+ 
+
